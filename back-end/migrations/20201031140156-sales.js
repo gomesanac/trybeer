c0dfb047f237a74bfd1296720d98c67adf4526b5
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-10-31 11:48:49 -0300

    migrations + seeds

---
+++ back-end/migrations/20201031140156-sales.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.createTable('Sales', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        references: { model: 'Users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL(9,2),
+        allowNull: false,
+      },
+      deliveryAddress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deliveryNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      saleDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    }),
+
+  down: async (queryInterface, _Sequelize) => queryInterface.dropTable('Sales'),
+};
