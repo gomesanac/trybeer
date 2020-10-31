8e14ccc08b68e8d165e0275be4db209a87d11dce
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-10-31 10:51:11 -0300

    altera estrutura connection

--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -11,23 +11,26 @@ const config = {
 
 let schema;
 
-const connection = () => (schema
-  ? Promise.resolve(schema)
-  : mysqlx
-    .getSession(config)
-    .then(async (session) => {
-      schema = await session.getSchema('Trybeer');
-      return schema;
-    })
-    .catch((err) => {
-      console.error(err);
-      process.exit(1);
-    }));
+const connection = () =>
+  schema
+    ? Promise.resolve(schema)
+    : mysqlx
+        .getSession(config)
+        .then(async (session) => {
+          schema = await session.getSchema('Trybeer');
+          return schema;
+        })
+        .catch((err) => {
+          console.error(err);
+          process.exit(1);
+        });
 
-const connNew = async (query) => (
-  mysqlx.getSession(config).then(async (session) => session.sql(query).execute())
+const connNew = async (query) =>
+  mysqlx
+    .getSession(config)
+    .then(async (session) => session.sql(query).execute())
     .catch((error) => {
       throw new Error(error.message);
-    })
-);
+    });
+
 module.exports = { connection, connNew };
